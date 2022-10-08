8afd07baf7b7ea64f034ad0396cd357ac0f595de
Florian <florianpaulschoene@gmail.com>
2022-10-08 17:22:16 +0200

    workingFix

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+  finalString = "";
+  if (num >= 0) {
+    for (let i = 0; i < num; i++) [(finalString += string)];
+    return finalString;
+  } else {
+    return "ERROR";
+  }
 };
 
 // Do not edit below this line
