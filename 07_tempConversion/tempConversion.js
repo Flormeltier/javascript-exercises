b78c35b713d2afe6f079b0d5fed6a361bfe8ff7d
Florian <florianpaulschoene@gmail.com>
2022-10-13 00:03:41 +0200

    tempConv working

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,18 @@
-const ftoc = function() {
-
+const roundedToFixed = function (input) {
+  let rounded = Math.pow(10, 1);
+  return Math.round(input * rounded) / rounded.toFixed(1);
 };
 
-const ctof = function() {
+const ftoc = function (tempInF) {
+  return roundedToFixed(((tempInF - 32) * 5) / 9);
+};
 
+const ctof = function (tempInC) {
+  return roundedToFixed((tempInC * 9) / 5 + 32);
 };
 
 // Do not edit below this line
 module.exports = {
   ftoc,
-  ctof
+  ctof,
 };
